f68d1a31a3dd8a4a902a3198142a1321da32c954
abhishekgohatre-KD4 <abhishekgohatre@gmail.com>
2023-10-17 23:31:14 +0530

    assignment06 uploaded

---
+++ Assignment_6/assign_6/src/com/abhishek/ExceptionLineTooLong.java
@@ -0,0 +1,29 @@
+package com.abhishek;
+
+public class ExceptionLineTooLong extends RuntimeException
+{
+	private String exceptionMsg;
+	
+	public ExceptionLineTooLong() 
+	{
+		
+	}
+
+	public ExceptionLineTooLong(String exceptionMsg) 
+	{	
+		this.exceptionMsg = exceptionMsg;
+	}
+	public void displayException()
+	{
+		System.out.println(this.exceptionMsg);
+	}
+
+	@Override
+	public String toString() {
+		return "ExceptionLineTooLong= " + exceptionMsg;
+	}
+	
+
+}
+
+
